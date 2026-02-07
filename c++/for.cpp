033bd26a48ebf77bd0b7dcd37b3f0f2d62fb408f
mashrursifar <mashrursifar@gmail.com>
2026-02-07 22:37:09 +0600

    start

---
+++ c++/for.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 10;
+
+    for(int x=0;x<i;++x)
+    {
+        cout <<"The number is: "<< x<<endl;
+
+    }
+    return 0;
+}
