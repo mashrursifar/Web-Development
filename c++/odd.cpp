e3ac33b22b1b3ea69b81502e36ac3f57f7cc8228
mashrursifar <mashrursifar@gmail.com>
2026-02-07 22:41:22 +0600

    odd numbers

---
+++ c++/odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 10;
+
+    for(int x=0;x<i;x++)
+    {
+        
+        if(x%2==1){
+            cout <<x<< "\t";
+        }
+    }
+    return 0;
+}
