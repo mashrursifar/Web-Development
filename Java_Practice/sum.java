21ab648bd9f9458536213c91165b6577f0cf31ff
mashrursifar <mashrursifar@gmail.com>
2026-02-09 19:47:52 +0600

    sum

---
+++ Java_Practice/sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sum {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 2 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+
+
+    
+
+    
+}
