1fbb9dfd3921a600fa006eb481b173b27028ef93
mashrursifar <mashrursifar@gmail.com>
2026-02-09 19:44:44 +0600

    update

--- Java_Practice/Test.java
+++ Java_Practice/Test.java
@@ -1,8 +1,17 @@
+import java.util.*;
 public class Test{
 
 
     public static void main(String args []){
         System.out.print("Hello! Sifar. How are you?\n");
         System.out.println("Yes, I am fine!!");
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean a = sc.nextBoolean();
+
+        System.out.println(a);
     }
 }
