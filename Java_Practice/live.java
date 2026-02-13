c9c2d56ea8f7ba86cfd0a9125a2b543e6ccfc104
mashrursifar <mashrursifar@gmail.com>
2026-02-13 21:57:20 +0600

    first problem solved

--- Java_Practice/live.java
+++ Java_Practice/live.java
@@ -6,16 +6,23 @@ import java.util.Scanner; public class live {
         System.out.print("How many numbers you want to give: ");
 
         int n = sc.nextInt();
-
+        int sum_even=0, sum_odd=0;
         for(int i=0; i<n; i++)
         {
             System.out.print("Enter a value: ");
             int num = sc.nextInt();
-            System.out.println("User Enterd: "+num);
+            if(num%2==0)
+            {
+                sum_even += num;
+            }else{
+                sum_odd += num;
+            }
         }
+        System.out.println("Sum of all even numbers: "+sum_even);
+        System.out.println("Sum of all odd numbers: "+sum_odd);
 
 
-
+        sc.close();
 
     }
 
