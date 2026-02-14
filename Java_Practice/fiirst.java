05bbe94ddd862a7d206beb81f4c0de1e52a4d32e
mashrursifar <mashrursifar@gmail.com>
2026-02-14 20:35:34 +0600

    DSA first problem

--- Java_Practice/fiirst.java
+++ Java_Practice/fiirst.java
@@ -1,11 +1,34 @@
 public class fiirst {
     
     public static void main(String[] args) {
-        int[] arr= {2,4,3,5};
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.print(arr[i]+"\t");
-        }
+        int[] arr= {2,1,3,5};
+        int target= 9;
+
+        solution sol = new solution();
+        int[] index= sol.twoSum(arr,target);
+        System.out.println(index[0]+" "+index[1]);
     }
 
 }
+
+class solution{
+    public int[] twoSum(int[] arr, int target)
+    {
+        int[] res=new int[2];
+
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                int sum = arr[i]+arr[j];
+                if(sum==target)
+                {
+                    res[0]=i;
+                    res[1]=j;
+                }
+            }
+        }
+
+        return res;
+    }
+} 
