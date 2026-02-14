6e616cfd17ede80676e830c174e6f5f2684017d0
mashrursifar <mashrursifar@gmail.com>
2026-02-14 20:14:17 +0600

    s

---
+++ Java_Practice/fiirst.java
@@ -0,0 +1,11 @@
+public class fiirst {
+    
+    public static void main(String[] args) {
+        int[] arr= {2,4,3,5};
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+"\t");
+        }
+    }
+
+}
