be41cc28268d87238f4db31483ab68b1414a527f
mashrursifar <mashrursifar@gmail.com>
2026-02-16 23:10:50 +0600

    one side done

--- Java_Practice/numberPyramid.java
+++ Java_Practice/numberPyramid.java
@@ -1,13 +1,27 @@
 public class numberPyramid {
     public static void main(String[] args) {
-        int n=5;
+        int n=4;
         int num=1;
 
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<=n;j++)
             {
-                System.out.print(num);
+                if(j<n-i)
+                {System.out.print(" ");}
+                else
+                {
+                    System.out.print(num);
+                    
+                }
+                // if(num>1)
+                //     {
+                //     for(int k=0;k<i;k++)
+                //     {
+                //         System.out.print(num);
+                //     }
+                //     }
+                
             }
             num++;
             System.out.println();
