609caa856f485a57d456938c8750d43e64334007
mashrursifar <mashrursifar@gmail.com>
2026-02-18 21:12:00 +0600

    holo squre done

--- Java_Practice/holoSqure.java
+++ Java_Practice/holoSqure.java
@@ -10,7 +10,17 @@ public class holoSqure {
 
         for(int i=0;i<n;i++)
         {
-            
+            for(int j=0;j<n; j++)
+            {
+                if(i==0 || j==0 || j==n-1 || i == n-1)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
 
         sc.close();
