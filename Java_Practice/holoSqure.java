f30e578287f5200668fa0fd4796603f315a07cf2
mashrursifar <mashrursifar@gmail.com>
2026-02-18 21:08:37 +0600

    holo Squre Start

---
+++ Java_Practice/holoSqure.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class holoSqure {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        for(int i=0;i<n;i++)
+        {
+            
+        }
+
+        sc.close();
+    }
+}
