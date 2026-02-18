967d6fec958a7bf81003902cfd3547f431fcce54
mashrursifar <mashrursifar@gmail.com>
2026-02-18 22:42:49 +0600

    floyd triangle

---
+++ Java_Practice/floydtriangle.java
@@ -0,0 +1,19 @@
+public class floydtriangle {
+    static void floydtriangle(int n)
+    {   
+        int count = 1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        floydtriangle(7);
+    }
+}
