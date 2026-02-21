d3e9184d09ae987c862de4f859677f2b9ac2c0c1
mashrursifar <mashrursifar@gmail.com>
2026-02-21 21:44:41 +0600

    just

---
+++ Java_Practice/mul.java
@@ -0,0 +1,17 @@
+public class mul {
+    
+    public static int multiply(int a,int b)
+    {
+        int res = a*b;
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int a = 34, b =3;
+
+        
+        System.out.println(multiply(a, b));
+    }
+}
