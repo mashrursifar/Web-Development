ec817def33cdf23aad136446e4b99746867c17ef
mashrursifar <mashrursifar@gmail.com>
2026-02-21 21:55:55 +0600

    prime checker

---
+++ Java_Practice/prime.java
@@ -0,0 +1,12 @@
+public class prime {
+    public static void primeCheck(int n)
+    {
+        if(n%2==1) System.out.println(n+" is a prime number");
+        else System.out.println(n+" is not a prime number");
+
+    }
+    
+    public static void main(String[] args) {
+        primeCheck(3543123);
+    }
+}
