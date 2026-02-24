efad8a5d1cb300c2e1cf71e0ee37bc08ba4b02ca
mashrursifar <mashrursifar@gmail.com>
2026-02-24 21:54:09 +0600

    Array part I done

---
+++ Java_Practice/subarr.java
@@ -0,0 +1,33 @@
+public class subarr {
+    public static void subArray(int arr[])
+    {
+        int sum = 0,ts=0, max = Integer.MIN_VALUE, min= Integer.MAX_VALUE;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i;j<arr.length;j++)
+            {   sum =0;
+                for(int k=i;k<=j;k++)
+                {
+                    System.out.print(arr[k]+" ");
+                    sum += arr[k];
+                }
+                ts++;
+                if(sum>max)max=sum;
+                if(sum<min)min=sum;
+                
+                System.out.println(" sum= "+sum);
+            }
+            System.out.println();
+        }
+        System.out.println("Total number of sub array= "+ts);
+        System.out.println("Maximum sum from the sub array= "+max);
+        System.out.println("Minimum sum from the sub array= "+min);
+    }
+    public static void main(String[] args) {
+        int arr[] = {10,13,5,-3,7,11};
+
+        subArray(arr);
+    }
+    
+}
