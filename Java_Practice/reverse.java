cf478418525df736cd1bc652d6bed6bdda245635
mashrursifar <mashrursifar@gmail.com>
2026-02-24 20:45:15 +0600

    reverse done

--- Java_Practice/reverse.java
+++ Java_Practice/reverse.java
@@ -1,3 +1,26 @@
 public class reverse {
-    
+    public static void rev(int rev[])
+    {
+        int first=0, last=rev.length-1, temp=0;
+
+        while (first<last) {
+            temp = rev[last];
+            rev[last] = rev[first];
+            rev[first] = temp;
+            first++;
+            last--;
+
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,36,5,4};
+
+        rev(arr);
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
