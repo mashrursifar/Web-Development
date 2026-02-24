a50de9249fdf0fb9d1c8121edeab69ea25d5b3b6
mashrursifar <mashrursifar@gmail.com>
2026-02-24 21:06:02 +0600

    pairs done

---
+++ Java_Practice/pairs.java
@@ -0,0 +1,20 @@
+public class pairs {
+    public static void pairPrint(int num[])
+    {
+        for(int i=0;i<num.length;i++)
+        {
+            for(int j=i;j<num.length;j++)
+            {
+                System.out.print("("+num[i]+","+num[j]+"),");
+
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,4,8,10};
+
+        pairPrint(arr);
+    }
+    
+}
