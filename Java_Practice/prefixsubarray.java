32a3d2af6c9a7f5f75a0bb36ea11605193103a14
mashrursifar <mashrursifar@gmail.com>
2026-02-27 17:29:08 +0600

    debi

--- Java_Practice/prefixsubarray.java
+++ Java_Practice/prefixsubarray.java
@@ -3,21 +3,29 @@ public class prefixsubarray {
     public static void preMax(int arr[])
     {
         int prefix[] = new int[arr.length];
-        int sum=0;
+        int sum=0, max = Integer.MIN_VALUE;
 
-        prefix[0] = arr[1];
+        prefix[0] = arr[0];
 
         for(int i=1; i<arr.length;i++)
         {
-            for(int j=1; j<arr.length;j++)
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        for(int i=1; i<arr.length;i++)
+        {
+            for(int j=i; j<arr.length;j++)
             {
-                prefix[j] = arr[i]+
+                sum = i==0 ? prefix[j]:prefix[j] -prefix[i-1];
+
+                if(max<sum) max=sum;
             }
         }
+        System.out.println("Max sum = "+max);
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,-2,5,4};
+        int arr[] = {7,-2,5,4,-1,4};
 
         preMax(arr);
 
